00b4718a98c54c2ab568c7894ed705e0a2983313
Josep Damià Carbonell Seguí <josepdcs@ext.inditex.com>
2022-12-07 16:53:49 +0100

    Pylint errors

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
+"""Instagram Tool"""
+
 import sys
 
-import click as click
+import click
 from instagrapi import Client
 
 
-@click.group(name="Instagram Swiss knife")
+@click.group(name="Instagram Tool")
 @click.version_option(version='0.1.0')
 def commands():
-    """Instagram Swiss knife - Manage your posted media, stories, etc. You will be able to back
+    """Instagrool - Manage your posted media, stories, etc. You will be able to back
     up of your media or to delete the oldest ones."""
 
 
@@ -34,23 +36,27 @@ def delete_command(username, password, size):
         print("Size (with the amount of media to be removed) is mandatory.")
         sys.exit(1)
 
-    cl = Client()
-    cl.login(username, password)
+    client = Client()
+    client.login(username, password)
 
-    user_id = cl.user_id_from_username(username)
-    medias = cl.user_medias(int(user_id), 0)
+    user_id = client.user_id_from_username(username)
+    medias = client.user_medias(int(user_id), 0)
     medias.reverse()
 
     i = 1
     for media in medias[:int(size)]:
         print(f"Trying to delete [{i}]: {media}")
-        cl.media_delete(media.pk)
+        client.media_delete(media.pk)
         i += 1
 
 
 @commands.command(name="backup")
 def backup_command(username, password, size):
     """Backup your media"""
+
+    print(username)
+    print(password)
+    print(size)
 
 
 # Press the green button in the gutter to run the script.
